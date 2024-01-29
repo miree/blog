cb0a03c8addc44b6634c32409a680533fd3bf566
Michael Reese <michaelate@gmail.com>
2024-01-29 17:47:58 +0100

    peak detection for linear regression filter

--- technical-stuff/digital-pulseshape-transformation/filtool.d
+++ technical-stuff/digital-pulseshape-transformation/filtool.d
@@ -62,8 +62,11 @@ void main(string[] args)
 			params  ~= arg[1..$].to!double;
 		}
 		if (arg.startsWith("r")) {
-			filters ~= new LinearRegression(arg[1..$].to!double);
-			params  ~= arg[1..$].to!double;
+			long comma_pos = -1;
+			foreach(i,ch;arg) if (ch==',') comma_pos = i;
+			filters ~= new LinearRegression(arg[1..comma_pos].to!double,
+				                            arg[comma_pos+1..$].to!double);
+			params  ~= arg[1..comma_pos].to!double;
 		}
 		if (arg.startsWith("gen:")) {
 			auto ff = FitFunc(filters);
@@ -273,77 +276,62 @@ class LinearRegression : Filter {
 	double xy_sum = 0.0;
 	double xy2_sum = 0.0;
 	int N = 0;
-	this (double w) {
+	double noise_level;
+
+	double min_b1 = 0;
+	int detect_countdown = 0;
+	double A_detected = 0;
+	double x_detected = 0;
+	int N_detected = 0;
+	double A_max = 0;
+	double dx_Amax = 0;
+	double A_rising = 0;
+
+	double peak0 = 0;
+	double peak1 = 0;
+	double peak2 = 0;
+
+	double peakt1 = 0;
+
+
+	double t = 0;
+
+	File* f;
+
+	this (double w, double noise) {
+		peak0 = 0;
+		peak1 = 0;
+		peak2 = 0;
+		peakt1 = 0;
+		f = new File("pulses.dat","w+");
+
+		t = 0;
+		
 		N = cast(int)w;
+		noise_level = noise;
+
 		buffer = new double[N];
 		buffer[] = 0.0;
 		buffer2 = new double[N];
 		buffer2[] = 0.0;
 		x_sum = 0.0;
 		xx_sum = 0.0;
-		foreach(i;0..N) {
-			x_sum  += i;
-			xx_sum += i*i;
-		}
-		y_sum  = 0.0;
-		y2_sum  = 0.0;
-		xy_sum = 0.0;
-		xy2_sum = 0.0;
-	}
-	override void reset(double w) {
-		N = cast(int)w;
-		buffer.length = N;
-		buffer[] = 0.0;
-		buffer2.length = N;
-		buffer2[] = 0.0;
-		idx = 0;
-		x_sum = 0.0;
-		xx_sum = 0.0;
-		foreach(i;0..N) {
-			x_sum  += i;
-			xx_sum += i*i;
-		}
-		y_sum  = 0.0;
-		y2_sum  = 0.0;
-		xy_sum = 0.0;
-		xy2_sum = 0.0;
-	}
-	double slope(double[] ys, int idx) {
-		double x_sum  = 0;
-		double y_sum  = 0;
-		double xx_sum = 0;
-		double xy_sum = 0;
-		int N = 0;
-		for(int x = 0; x < ys.length; ++x) {
-			int i = idx+x;
-			if (i>=ys.length) i-=ys.length;
-			double y = ys[i];
-			x_sum += x;
-			y_sum += y;
+		foreach(x;0..N) {
+			x_sum  += x;
 			xx_sum += x*x;
-			xy_sum += x*y;
-			++N;
 		}
-		double D = xx_sum*N - x_sum*x_sum;
-		return  (xy_sum*N - x_sum*y_sum) / D;
-		//return xy_sum;
+		y_sum  = 0.0;
+		y2_sum  = 0.0;
+		xy_sum = 0.0;
+		xy2_sum = 0.0;
 	}
-//                              y_sum -vvvvvvvvvvvvvvvvv
-// 0*y0 + 1*y1 + 2*y2 + 3*y3[+ 4*y4 - (y0 + y1 + y2 + y3 + y4 - y0) ]
-//        0*y1 + 1*y2 + 2*y3 + 3*y4[+ 4*y5 - (y2 + y3 + y4 + y5) ]
-//               0*y2 + 1*y3 + 2*y4 + 3*y5 
-// => xy_sum += (N-1)*y - y_sum;
-// => y_sum += y-buffer[idx];
-// => buffer[idx] = y; 
-
-
-// [0,0,0,0] y_sum = 0; xy_sum = 0
-// [1,0,0,0] y_sum = 1; xy_sum = 4	
-// [1,2,0,0] y_sum = 3; xy_sum = 4+8	
-// [1,2,3,0] y_sum = 6; xy_sum = 0	
-// [1,2,3,4] y_sum =10; xy_sum = 0	
-// [5,2,3,4] y_sum =14; xy_sum = 0	
+	override void reset(double dummy) {
+		stderr.writeln("LinearRegression filter cannot be used for fitting");
+		assert(false);
+	}
 	override double apply(double y) {
+		t+=1.0;
+
 
 		y2_sum  += buffer[idx]-buffer2[idx];
 		xy2_sum += N*buffer[idx] - y2_sum;
@@ -353,24 +341,42 @@ class LinearRegression : Filter {
 		xy_sum += N*y - y_sum;
 		buffer[idx] = y;
 
+
 		if (++idx >= N) idx = 0;
 
-		//double slope1 = slope(buffer, idx);
-		//double slope2 = slope(buffer2);
-
-
-
-
 		double D = (xx_sum*N - x_sum*x_sum);
-		double b1 = (xy_sum*N - x_sum*y_sum)/D;
-		double a1 = y_sum/N - b1 * x_sum/N;
-		double b2 = (xy2_sum*N - x_sum*y2_sum)/D;
-		double a2 = y2_sum/N - b2 * x_sum/N;
+		double b1 = (xy_sum*N - x_sum*y_sum)/D; // slope
+		double a1 = y_sum/N - b1 * x_sum/N;     // y-axis intersection
 
-		double dx1 = -(0*N+(b1*N+a1)/b1);
-		double dx2 = -(0*N+a2/b2);
-		double dx = 0.5*(dx1+dx2);
-		stderr.writeln(-b1*N, " ", b2*N, " ", (y_sum+y2_sum)/N, " ", dx);
+		double b2 = (xy2_sum*N - x_sum*y2_sum)/D; // slope
+		double a2 = y2_sum/N - b2 * x_sum/N;      // y-axis intersection
+
+		double dx1 = -(0*N+(b1*N+a1)/b1); // sub-sample x-position of falling side
+		double dx2 = -(0*N+a2/b2);        // sub-sample x-position of rising edge
+		double dx  = 0.5*(dx1+dx2);
+
+		// different amplitude estimates
+		double y1 = -b1*N;
+		double y2 =  b2*N;
+		double y3 = (y_sum+y2_sum)/N;
+
+		// find local maximum
+		peak2=peak1;
+		peak1=peak0;
+		peak0=(y1+y2+y3)/3.0;
+		if (peak0 < peak1 && peak1 >= peak2 && (N+(peakt1-t)>=-2.0 && N+(peakt1-t)<=2.0) ) { // local maximum in input N samples ago
+			//double tp = t-N+dx_Amax;
+			f.writeln(peakt1-2," 0");
+			f.writeln(peakt1-1," ", peak1, " ", N+(peakt1-t));
+			f.writeln(peakt1-0," 0");
+		}
+		A_rising = 0;
+
+		// remember previous peak time
+		peakt1 = t+dx-N+1;
+
+
+		stderr.writeln(y1, " ", y2, " ", y3, " ", dx);
 
 		return xy_sum;
 	}
